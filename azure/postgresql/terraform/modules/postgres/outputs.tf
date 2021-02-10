3281d3e8cf207767b3fffded61fe1e03408fe403
Daniela-G Zheleva <daniela-g.zheleva@db.com>
2021-02-10 14:09:09 +0100

    Merge pull request #9 in CIBCTO/fdx-cloud-service-certification from CIBCTO/fdx-cloud-service-certification-working:release/az-postgresql-tf to release/80-az-postgresql-tf
    Squashed commit of the following:
    
    commit 0aa57e2dbc3bee6d556345050c3effee1cd65293
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Wed Jan 27 14:46:31 2021 +0000
    
        resource provider
    
    commit 8e4db96551f0b7327c3b274d0a569eb362eefdf6
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Wed Jan 27 14:45:06 2021 +0000
    
        files dropped out of git when renaming module
    
    commit 28d18649f6b35dac0f721788b542dceaf45fe459
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Wed Jan 27 14:43:53 2021 +0000
    
        PR changes
    
    commit d7a7bc6d8cbb63b20eeaeccecdf85501273cec25
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Mon Jan 25 10:33:16 2021 +0000
    
        syntax
    
    commit a5368995af0ab0480fc54ae558b8d1f35ea4db31
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Mon Jan 25 10:32:30 2021 +0000
    
        add description to vars2
    
    commit e44bf0022f47fd9866d5e5700f20ec98384a692e
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Mon Jan 25 10:31:05 2021 +0000
    
        add description to vars
    
    commit b9ce96a1fed3439f2bda734f7c9ddfb79db3e0a1
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Mon Jan 25 10:30:04 2021 +0000
    
        delete non module variables
    
    commit 18bbf2c7cec469e317f67e98da8fb5305c58499c
    Merge: 21ba7c5 44ae054
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Mon Jan 25 09:54:39 2021 +0000
    
        Merge branches 'master' and 'release/az-postgresql-tf' of ssh://stash.gto.intranet.db.com:7998/cibcto/fdx-cloud-service-certification-working into release/az-postgresql-tf
    
    commit 21ba7c55535c5165653aa858d786e4543878bd2d
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Fri Jan 15 15:23:44 2021 +0000
    
        readme clarification
    
    commit a2ec5615cf65220c754129f31eac5a50fa86b856
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Fri Jan 15 14:54:36 2021 +0000
    
        edit readme
    
    commit cb760a4cb117c282da23211e97aa4614db942941
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Fri Jan 15 14:25:48 2021 +0000
    
        remove .idea
    
    commit bfe591fea9fe63ae1fcd440153eca05c7cb25afd
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Fri Jan 15 14:22:01 2021 +0000
    
        add accidentally deleted files
    
    commit 1a921dcbda925264ab0e6fd81c06aae93e41a945
    Author: Daniela-G Zheleva <daniela-g.zheleva@db.com>
    Date:   Fri Jan 15 14:19:39 2021 +0000
    
        copy tf over

---
+++ azure/postgresql/terraform/modules/postgres/outputs.tf
@@ -0,0 +1,13 @@
+output "server_fqdn" {
+  description = "The fully qualified domain name (FQDN) of the PostgreSQL server"
+  value       = "${azurerm_postgresql_server.postgres-server.fqdn}"
+}
+output "postgres_private_ip" {
+  value = "${azurerm_private_endpoint.postgres-endpoint.private_service_connection[0].private_ip_address}"
+}
+output "username" {
+  value = "${azurerm_postgresql_server.postgres-server.administrator_login}"
+}
+output "dbname" {
+  value = "${azurerm_postgresql_database.postgres-db.name}"
+}
