90746551e97e556c696c68ea393e6c9db0a277c5
Eddie Knight <knight@linux.com>
2024-08-04 17:28:46 -0500

    refinement
    Signed-off-by: Eddie Knight <knight@linux.com>

--- GCP/GCS/src/main.go
+++ GCP/GCS/src/main.go
@@ -23,22 +23,24 @@ var funcMap = map[string]interface{}{
 	"GCS_CCC_OS_C5_TR01": GCS_CCC_OS_C5_TR01,
 	"GCS_CCC_OS_C5_TR02": GCS_CCC_OS_C5_TR02,
 	"GCS_CCC_OS_C5_TR03": GCS_CCC_OS_C5_TR03,
-	
 }
 
 // Pass takes any number of strings and prints them as a single string
 func CFIPass(values ...string) {
 	cfiPrint(values, "PASS")
+	os.Exit(0)
 }
 
 // FAIL takes any number of strings and prints them as a single string
 func CFIFAIL(values ...string) {
 	cfiPrint(values, "FAIL")
+	os.Exit(1)
 }
 
 // Error takes any number of strings and prints them as a single string
 func CFIError(values ...string) {
 	cfiPrint(values, "ERROR")
+	os.Exit(2)
 }
 
 // cfiPrint prints the provided values as a single string with a prefix
@@ -50,15 +52,12 @@ func cfiPrint(values []string, prefix string) {
 func main() {
 	if len(os.Args) == 1 {
 		CFIError("Please provide a function name to call")
-		return
 	}
 
 	funcName := os.Args[1]
 	if function, exists := funcMap[funcName]; exists {
 		reflect.ValueOf(function).Call(nil)
 	} else {
-		CFIError("Function does not exist")
-		// panic and print function name if it does not exist
 		CFIError("Function %s does not exist", funcName)
 	}
 }
